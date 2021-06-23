da665870a3ec638607921628baa0a79f4d6f1a7f
nareshgurung <47278496+nareshgurung@users.noreply.github.com>
2021-06-23 14:37:01 -0400

    From Naresh

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    //this is a comment 
+    /*
+    this is a multiline comment. comments are not in our program  and they do not affect how the program runs.
+    */
+    // between the {}after the class declatation 
+    //defines the body of the class.
+
+    // The main method. 
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
