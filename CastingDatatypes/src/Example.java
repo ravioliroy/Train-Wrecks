9d53be2eff45c017488d2f7c7d61b63f78dec380
nareshgurung <47278496+nareshgurung@users.noreply.github.com>
2021-06-24 10:28:14 -0400

    castingDatatypes.Naresh

--- CastingDatatypes/src/Example.java
+++ CastingDatatypes/src/Example.java
@@ -1,29 +1,17 @@
+
 public class Example {
-
-    public static void main(String[] args) {
-
-        // default values
-
-        int i = 200;
-
-        // cast to a short
-
-        short s = (short) i;
-
-        System.out.println(s);
-
-        // cast to a double
-
-        double d = (double) i;
-
-        System.out.println(d);
-
-        // cast to a byte
-
-        byte b = (byte) i;
-
-        System.out.println(b);
-
-    }
-
+public static void main(String[] args) {
+	//intial values
+	int i = 200;
+	//cast to a short
+	short s =(short)i;
+	System.out.println(s);
+	
+	//cast to a double
+	double d=(double)s;
+	System.out.println(d);
+	//cast to a byte
+	byte b =(byte)i;
+	System.out.println(b);
+}
 }
