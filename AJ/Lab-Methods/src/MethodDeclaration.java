b5f05864f0cd7197e537f1db905269cf68baf49e
ravioliroy <beezlebobster@gmail.com>
2021-06-25 09:28:04 -0500

    Labs Day 4

---
+++ AJ/Lab-Methods/src/MethodDeclaration.java
@@ -0,0 +1,15 @@
+public class MethodDeclaration {
+    public static void main(String[] args) {
+        // create a class instance
+        MethodDeclaration md = new MethodDeclaration();
+
+        // call your first method here
+        md.talk();
+        md.talk();
+        md.talk();
+    }
+
+    public void talk() {
+        System.out.println("Inside of the talk method");
+    }
+}
